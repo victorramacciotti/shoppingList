2796e3f8d814e5b6673358d00f8e4bbb13d3c504
Victor Araujo <victtordan10@gmail.com>
2024-11-03 17:05:46 -0300

    atualização das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AppComponent } from './app.component';
 import { authGuardFn } from '@auth0/auth0-angular';
+import { HomeComponent } from './components/home/home.component';
+import { MyListComponent } from './components/my-list/my-list.component';
 
 export const routes: Routes = [
-    { path: 'home', component: AppComponent },
+    { path: '', component: HomeComponent },
+    { path: 'myList', component: MyListComponent, canActivate: [authGuardFn] },
     { path: 'profile', component: ProfileComponent, canActivate: [authGuardFn] },
     { path: '**', redirectTo: '/home' },
 ];
