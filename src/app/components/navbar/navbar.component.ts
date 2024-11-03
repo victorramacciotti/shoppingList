43e4a7f6874d561dedd4d4b4255178f6561cf7d2
Victor Araujo <victtordan10@gmail.com>
2024-11-03 11:21:22 -0300

    feat: criação e edição recurso navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [AsyncPipe, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  
+  constructor(public auth: AuthService) {}
+
+
+  login() {
+
+  this.auth.loginWithRedirect();
+  }
+
+  
+}
