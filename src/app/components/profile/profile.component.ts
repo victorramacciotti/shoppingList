82b37f651d22bfd9f83ea3c50e30c10e4e5135f9
Victor Araujo <victtordan10@gmail.com>
2024-11-03 17:04:57 -0300

    correção no profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService, User } from '@auth0/auth0-angular';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NavbarComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
